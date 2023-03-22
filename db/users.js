c3f4a9957ca4c5fd064b1fdad03c7621da88c62a
Sarah <sarah.celiann@gmail.com>
2023-03-22 09:57:53 -0500

    getUser passing all tests

--- db/users.js
+++ db/users.js
@@ -1,3 +1,4 @@
+const { RowDescriptionMessage } = require("pg-protocol/dist/messages");
 const client = require("./client");
 
 // database functions
@@ -15,7 +16,6 @@ async function createUser({ username, password }) {
     SELECT id, username FROM users
     WHERE username=$1;
     `, [username]);
-    console.log("ROWS", user);
     return user;
   } catch (err) {
     throw err;
@@ -24,7 +24,21 @@ async function createUser({ username, password }) {
 }
 
 async function getUser({ username, password }) {
-
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT * FROM users
+      WHERE username=$1;
+    `, [username]);
+    console.log(user);
+    if(user.password === password) {
+      delete user.password;
+      return user;
+    } else {
+      console.error("Password does not match");
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
