55d9fcaf675574c60221108f06d11826adf21ab0
Emily Cooke <emilycooke@Emilys-MacBook-Pro.local>
2023-03-30 08:59:58 -0600

    committing

--- app.js
+++ app.js
@@ -1,105 +1,22 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT || 3000;
 
 const apiRouter = require("./api")
+app.use("/api", apiRouter)
 // Setup your Middleware and API Router here
 
-module.exports = app;
-
-
 app.get('/', (req, res) => {
-  res.send(`
-  <h1>Fitness Tracker</h1>`)
+  res.send('Api router');
+  res.end();
 })
 
-app.get('/api/users/login', (req, res) => {
-  res.send(`
-<h1>user register<h1/>
-<form methood="POST">
-<input type="text" name="userName" placeholder="Username" />
-<input type="password" name="password" placeholder="password" />
-<button type="submit">Submit</button>
-</form>
-`)
-});
+app.listen(PORT, (err) => {
+  if (err) console.log('problem with server')
+  console.log(`listening on port ${PORT}`)
+})
 
-app.post("/users/register", (req, res) => {
-  res.send(`
-  <h1>Login<h1/>
-<form methood="POST">
-<input type="text" name="userName" placeholder="Username" />
-<input type="password" name="password" placeholder="password" />
-<input type="password" name="confirmPassword" placeholder="Confirm Password" />
-<button type="submit">Submit</button>
-</form>
-  `)
-});
+app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.send(`
-  <h1>Health</h1>
-  `)
-});
-
-//must be logged in
-app.get("/users/me", (req, res) => {
-  res.send(`
-  <h1>My Profile</h1>
-  `)
-});
-
-app.get("/users/:username/routines", (req, res) => {
-  res.send(`
-  <h1>My Routines</h1>
-  `)
-});
-
-//activites
-app.get("/activities", (req, res) => {
-  res.send(`
-  <h1>Activities</h1>
-  `)
-});
-
-//must be logged in
-app.post("/activities", () => {
-  res.send(`
-  `)
-});
-
-//patch activities/:activityId
-
-app.get("/activities/:activityId/routines", (req, res) => {
-  res.send(`
-  <h1>activity id/routines</h1>
-  `)
-});
-
-//routines
-
-app.get("/routines", (req, res) => {
-  res.send(`
-  <h1>My Routines</h1>
-  `)
-});
-
-//must be logged in
-app.post("/routines", () => {
-  res.send(`
-  `)
-});
-
-//PATCH /routines/:routineId
-
-//DELETE / routines /: routineId
-
-app.post("/routines/:routineId/activities", () => {
-  res.send(`
-  `)
-});
-
-//routine activities
-
-//PATCH /routine_activities/:routineActivityId
-//DELETE /routine_activities/:routineActivityId
+module.exports = app;
