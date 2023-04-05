e6a56b31f30f455da49834a8144fe572db7bfd64
Sarah <sarah.celiann@gmail.com>
2023-04-05 11:34:42 -0500

    api/routineActivities passing half tests

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+const { getRoutineActivityById } = require('../db/routine_activities');
+const { getRoutineById, getUserById, updateRoutineActivity } = require('../db');
 
 // PATCH /api/routine_activities/:routineActivityId
 router.patch('/:routineActivityId', async (req, res, next) => {
   const { routineActivityId } = req.params;
   const { routineId, activityId, duration, count } = req.body;
+  const prefix = 'Bearer ';
+  const auth = req.header('authorization');
+  const token = auth.slice(prefix.length);
+  const { id } =  jwt.verify(token, JWT_SECRET);
 
   const updateR = {};
 
@@ -21,13 +29,19 @@ router.patch('/:routineActivityId', async (req, res, next) => {
     updateR.count = count;
   }
 
-  try {
-    const originalRoutineActivity = await getRoutineActivityById(req.params);
+  updateR.id = routineActivityId;
+  console.log("UPDATR", updateR);
 
-    if (originalRoutineActivity.routineActivityId === routineActivityId) {
-      const updatedRoutineActivity = await updateRoutineActivity(routineActivityId, updateR)
-      res.send(updatedRoutineActivity);
-    }
+  try {
+    const originalRoutineActivity = await getRoutineActivityById(routineActivityId);
+    const routine = await getRoutineById(originalRoutineActivity.routineId);
+    const user = await getUserById(id);
+    if (routine.creatorId !== id) {
+      res.send({error: "UserDoesNotMatch", name: "UserDoesNotMatch", message: `User ${user.username} is not allowed to update ${routine.name}`});
+    } 
+    const updatedRoutine = await updateRoutineActivity(updateR);
+    console.log(updatedRoutine);
+    res.send(updatedRoutine);
   } catch ({ name, message }) {
     next({ name, message });
   }
