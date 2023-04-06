af9ee6fcf4b418b33907714d03e872be25e880dc
emilycooke <emcooke.esc@gmail.com>
2023-04-06 08:34:31 -0600

    pushing front end

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  }
+}
